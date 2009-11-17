f34ef8d44c894d77fd4ee54002dd720d0eb533eb
Emmanuel Bouton <ebouton@ebouton1.(none)>
2009-11-17 17:35:47 +0100

    Loading Config file and environment

--- library/Rest/Application.php
+++ library/Rest/Application.php
@@ -1,14 +1,14 @@
 <?php
+
+require_once 'Zend/Config/Ini.php';
+require_once 'Zend/Config/Xml.php';
+
 /**
- * Rest Framework
- * 
- * @package	Rest
- * @author	Emmanuel Bouton
- * @version	$Id: $
+ * Rest Application
+ * @author Emmanuel Bouton
  */
 class RestApplication
 {
-
     /**
      * Application environment
      * 
@@ -17,18 +17,47 @@ class RestApplication
     protected $_environment;
 
     /**
+     * Flattened (lowercase) option keys
+     * 
+     * @var array
+     */
+    protected $_optionKeys = array();
+
+    /**
+     * Options for RestApplication
+     * 
+     * @var array
+     */
+    protected $_options = array();
+
+    /**
      * Constructor
      *
-     * Initialize application.
+     * Initialize application. Potentially initializes include_paths, PHP 
+     * settings, and bootstrap class.
      * 
      * @param  string                   $environment 
      * @param  string|array|Zend_Config $options String path to configuration file, or array/Zend_Config of configuration options
-     * @throws Rest_Exception When invalid options are provided
+     * @throws RestException When invalid options are provided
      * @return void
      */
     public function __construct($environment, $options = null)
     {
-		$this->_environment = (string) $environment;
+        $this->_environment = (string) $environment;
+
+        if (null !== $options) {
+            if (is_string($options)) {
+                $options = $this->_loadConfig($options);
+            }
+            elseif ($options instanceof Zend_Config) {
+                $options = $options->toArray();
+            }
+            elseif (!is_array($options)) {
+                throw new RestException('Invalid options provided; must be location of config file, a config object, or an array');
+            }
+
+            $this->setOptions($options);
+        }
     }
 
     /**
@@ -40,5 +69,166 @@ class RestApplication
     {
         return $this->_environment;
     }
+
+    /**
+     * Set application options
+     * 
+     * @param  array $options 
+     * @return RestApplication
+     */
+    public function setOptions(array $options)
+    {
+        if (!empty($options['config'])) {
+            $options = $this->mergeOptions($options, $this->_loadConfig($options['config']));
+        }
+        
+        $this->_options = $options;
+
+        $options = array_change_key_case($options, CASE_LOWER);
+
+        $this->_optionKeys = array_keys($options);
+
+        if (!empty($options['phpsettings'])) {
+            $this->setPhpSettings($options['phpsettings']);
+        }
+        
+        if (!empty($options['includepaths'])) {
+            $this->setIncludePaths($options['includepaths']);
+        }
+        
+        return $this;
+    }
+
+    /**
+     * Retrieve application options (for caching)
+     * 
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->_options;
+    }
+
+    /**
+     * Is an option present?
+     * 
+     * @param  string $key 
+     * @return bool
+     */
+    public function hasOption($key)
+    {
+        return in_array(strtolower($key), $this->_optionKeys);
+    }
+
+    /**
+     * Retrieve a single option
+     * 
+     * @param  string $key 
+     * @return mixed
+     */
+    public function getOption($key)
+    {
+        if ($this->hasOption($key)) {
+            $options = $this->getOptions();
+            $options = array_change_key_case($options, CASE_LOWER);
+            return $options[strtolower($key)];
+        }
+        return null;
+    }
+
+    /**
+     * Merge options recursively
+     * 
+     * @param  array $array1 
+     * @param  mixed $array2 
+     * @return array
+     */
+    public function mergeOptions(array $array1, $array2 = null)
+    {
+        if (is_array($array2)) {
+            foreach ($array2 as $key => $val) {
+                if (is_array($array2[$key])) {
+                    $array1[$key] = (array_key_exists($key, $array1) && is_array($array1[$key]))
+                                  ? $this->mergeOptions($array1[$key], $array2[$key]) 
+                                  : $array2[$key];
+                } else {
+                    $array1[$key] = $val;
+                }
+            }
+        }
+        return $array1;
+    }
+
+    /**
+     * Set PHP configuration settings
+     * 
+     * @param  array $settings 
+     * @param  string $prefix Key prefix to prepend to array values (used to map . separated INI values)
+     * @return RestApplication
+     */
+    public function setPhpSettings(array $settings, $prefix = '')
+    {
+        foreach ($settings as $key => $value) {
+            $key = empty($prefix) ? $key : $prefix . $key;
+            if (is_scalar($value)) {
+                ini_set($key, $value);
+            }
+            elseif (is_array($value)) {
+                $this->setPhpSettings($value, $key . '.');
+            }
+        }
+        
+        return $this;
+    }
+
+    /**
+     * Set include path
+     * 
+     * @param  array $paths 
+     * @return RestApplication
+     */
+    public function setIncludePaths(array $paths)
+    {
+        $path = implode(PATH_SEPARATOR, $paths);
+        set_include_path($path . PATH_SEPARATOR . get_include_path());
+        return $this;
+    }
+
+    /**
+     * Run the application
+     * 
+     * @return void
+     */
+    public function run()
+    {
+        // TODO
+    }
+
+    /**
+     * Load configuration file of options
+     * 
+     * @param  string $file
+     * @throws RestException When invalid configuration file is provided 
+     * @return array
+     */
+    protected function _loadConfig($file)
+    {
+        $environment = $this->getEnvironment();
+        $suffix      = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+        
+        switch ($suffix) {
+            case 'ini':
+                $config = new Zend_Config_Ini($file, $environment);
+                break;
+                
+            case 'xml':
+                $config = new Zend_Config_Xml($file, $environment);
+                break;
+                
+            default:
+                throw new RestException('Invalid configuration file provided; unknown config type');
+        }
+        
+        return $config->toArray();
+    }
 }
-?>
--- tests/library/Rest/ApplicationTest.php
+++ tests/library/Rest/ApplicationTest.php
@@ -1,13 +1,222 @@
 <?php
-require_once 'Rest/Application.php';
- 
-class ApplicationTest extends PHPUnit_Framework_TestCase {
 
-    public function testRecordingEnvironment()
+/** RestApplication */
+require_once 'Rest/Application.php';
+require_once 'Rest/Exception.php';
+
+class RestApplicationTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
     {
-		$application = new RestApplication("development");
-		$this->assertNotNull($application);
-        $this->assertEquals("development", $application->getEnvironment());
+        // Store original include_path
+        $this->includePath = get_include_path();
+        $this->application = new RestApplication('testing');
+        $this->iniOptions = array();
     }
+
+    public function tearDown()
+    {
+        foreach ($this->iniOptions as $key) {
+            ini_restore($key);
+        }
+    }
+
+    /** @test */
+    public function constructorShouldSetsEnvironment()
+    {
+        $this->assertEquals('testing', $this->application->getEnvironment());
+    }
+
+    /** @test */
+    public function constructorShouldSetOptionsWhenProvided()
+    {
+        $options = array(
+            'foo' => 'bar',
+            'bar' => 'baz',
+        );
+        $application = new RestApplication('testing', $options);
+        $this->assertEquals($options, $application->getOptions());
+    }
+
+    /** @test */
+    public function hasOptionShouldReturnFalseWhenOptionNotPresent()
+    {
+        $this->assertFalse($this->application->hasOption('foo'));
+    }
+
+    /** @test */
+    public function hasOptionShouldReturnTrueWhenOptionPresent()
+    {
+        $options = array(
+            'foo' => 'bar',
+            'bar' => 'baz',
+        );
+        $application = new RestApplication('testing', $options);
+        $this->assertTrue($application->hasOption('foo'));
+    }
+
+    /** @test */
+    public function getOptionShouldReturnNullWhenOptionNotPresent()
+    {
+        $this->assertNull($this->application->getOption('foo'));
+    }
+
+    /** @test */
+    public function getOptionShouldReturnOptionValue()
+    {
+        $options = array(
+            'foo' => 'bar',
+            'bar' => 'baz',
+        );
+        $application = new RestApplication('testing', $options);
+        $this->assertEquals($options['foo'], $application->getOption('foo'));
+    }
+
+    /** @test */
+    public function passingIncludePathOptionShouldModifyIncludePath()
+    {
+        $expected = dirname(__FILE__) . '/_files';
+        $this->application->setOptions(array(
+            'includePaths' => array(
+                $expected,
+            ),
+        ));
+        $test = get_include_path();
+        $this->assertContains($expected, $test);
+    }
+
+    /** @test */
+    public function passingPhpSettingsShouldSetsIniValues()
+    {
+        $this->iniOptions[] = 'y2k_compliance';
+        $orig     = ini_get('y2k_compliance');
+        $expected = $orig ? 0 : 1;
+        $this->application->setOptions(array(
+            'phpSettings' => array(
+                'y2k_compliance' => $expected,
+            ),
+        ));
+        $this->assertEquals($expected, ini_get('y2k_compliance'));
+    }
+
+    /** @test */
+    public function passingPhpSettingsAsArrayShouldConstructDotValuesAndSetRelatedIniValues()
+    {
+        $this->iniOptions[] = 'date.default_latitude';
+        $orig     = ini_get('date.default_latitude');
+        $expected = '1.234';
+        $this->application->setOptions(array(
+            'phpSettings' => array(
+                'date' => array(
+                    'default_latitude' => $expected,
+                ),
+            ),
+        ));
+        $this->assertEquals($expected, ini_get('date.default_latitude'));
+    }
+
+    /**
+     * @test
+     * @expectedException RestException
+     */
+    public function passingInvalidOptionsArgumentToConstructorShouldRaiseException()
+    {
+        $application = new RestApplication('testing', new stdClass());
+    }
+
+    /** @test */
+    public function passingStringIniConfigPathOptionToConstructorShouldLoadOptions()
+    {
+        $application = new RestApplication('testing', dirname(__FILE__) . '/appconfig.ini');
+        $this->assertTrue($application->hasOption('foo'));
+    }
+
+    /** @test */
+    public function passingStringXmlConfigPathOptionToConstructorShouldLoadOptions()
+    {
+        $application = new RestApplication('testing', dirname(__FILE__) . '/appconfig.xml');
+        $this->assertTrue($application->hasOption('foo'));
+    }
+
+    /** @test */
+    public function passingArrayOptionsWithConfigKeyShouldLoadOptions()
+    {
+        $application = new RestApplication('testing', array('bar' => 'baz', 'config' => dirname(__FILE__) . '/appconfig.ini'));
+        $this->assertTrue($application->hasOption('foo'));
+        $this->assertTrue($application->hasOption('bar'));
+    }
+
+    /**
+     * @test
+     * @expectedException RestException
+     */
+    public function passingInvalidStringOptionToConstructorShouldRaiseException()
+    {
+        $application = new RestApplication('testing', dirname(__FILE__) . '/appconfig');
+    }
+
+    /** @test */
+    public function passingZendConfigToConstructorShouldLoadOptions()
+    {
+        $config = new Zend_Config_Ini(dirname(__FILE__) . '/appconfig.ini', 'testing');
+        $application = new RestApplication('testing', $config);
+        $this->assertTrue($application->hasOption('foo'));
+    }
+
+    /** @test */
+    public function passingArrayOptionsToConstructorShouldLoadOptions()
+    {
+        $config = new Zend_Config_Ini(dirname(__FILE__) . '/appconfig.ini', 'testing');
+        $application = new RestApplication('testing', $config->toArray());
+        $this->assertTrue($application->hasOption('foo'));
+    }
+
+    /** @test */
+    public function optionsShouldRetainOriginalCase()
+    {
+        $options = array(
+            'pluginPaths' => array(
+                'RestApplication_Test_Path' => dirname(__FILE__),
+            ),
+            'Resources' => array(
+                'foo' => array(),
+                'Bar' => array(
+                    'baseUrl'             => '/foo',
+                    'moduleDirectory'     => dirname(__FILE__),
+                ),
+            ),
+        );
+        $this->application->setOptions($options);
+        $setOptions = $this->application->getOptions();
+        $this->assertSame(array_keys($options), array_keys($setOptions));
+    }
+
+    /** @test */
+    public function setOptionsShouldProperlyMergeTwoConfigFileOptions()
+    {
+        $application = new RestApplication('production', dirname(__FILE__) . '/appconfig-1.ini');
+        $options = $application->getOptions();
+        $this->assertEquals(array('config', 'includePaths'), array_keys($options));
+    }
+
+    /** @test */
+    public function hasOptionShouldTreatOptionKeysAsCaseInsensitive()
+    {
+        $application = new RestApplication('production', array(
+            'fooBar' => 'baz',
+        ));
+        $this->assertTrue($application->hasOption('FooBar'));
+    }
+
+    /** @test */
+    public function getOptionShouldTreatOptionKeysAsCaseInsensitive()
+    {
+        $application = new RestApplication('production', array(
+            'fooBar' => 'baz',
+        ));
+        $this->assertEquals('baz', $application->getOption('FooBar'));
+    }
+
 }
-?>
+
